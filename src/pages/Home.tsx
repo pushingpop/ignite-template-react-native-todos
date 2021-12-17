e83d3d61b5af01c2e40fde9c0880838fafb3f10f
800006276 <raphael.dantas@sonda.com>
2021-12-17 01:33:34 -0300

    Challenge Complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const item = updatedTasks.find(item => item.id === id)
+
+    if (!item)
+    return
+
+    item.done = !item.done
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(updatedTasks)
   }
 
   return (
